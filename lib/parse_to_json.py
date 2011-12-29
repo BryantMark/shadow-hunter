d72a0be20f2254b6819cbd278aafb6ed6196f7f0
ftiasch <ftiasch0@gmail.com>
2011-12-30 03:58:47 +0800

    parse ejudge style into json

---
+++ lib/parse_to_json.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import sys
+import json
+
+if __name__ == "__main__":
+    team = {}
+    submission = []
+    for line in sys.stdin:
+        if line[0] == '@':
+            line = line[1:]
+            command_type = line.split()[0]
+            line = ' '.join(line.split()[1:])
+            if command_type == 't':
+                team_id = line.split(',')[0]
+                team_name = ','.join(line.split(',')[3:])[1:-1]
+                team[team_id] = team_name
+            if command_type == 's':
+                team_id, problem_code, submit_count, submit_time, \
+                        result_code = line.split(',')[0:5]
+                team_name = team[team_id]
+                submit_time = int(submit_time)
+                submission.append((team_name, problem_code, \
+                        submit_time, result_code))
+    sys.stdout.write(json.dumps(submission, indent=4))
