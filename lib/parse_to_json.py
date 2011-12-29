9e4ed44d2ab5bd07f5fa0c370118228a42cf55d1
ftiasch <ftiasch0@gmail.com>
2011-12-30 07:15:20 +0800

    parse the number of problem

--- lib/parse_to_json.py
+++ lib/parse_to_json.py
@@ -4,6 +4,7 @@ import sys
 import json
 
 if __name__ == "__main__":
+    problem_count = 0
     team = {}
     submission = []
     for line in sys.stdin:
@@ -11,6 +12,8 @@ if __name__ == "__main__":
             line = line[1:]
             command_type = line.split()[0]
             line = ' '.join(line.split()[1:])
+            if command_type == 'p':
+                problem_count += 1
             if command_type == 't':
                 team_id = line.split(',')[0]
                 team_name = ','.join(line.split(',')[3:])[1:-1]
@@ -22,4 +25,5 @@ if __name__ == "__main__":
                 submit_time = int(submit_time)
                 submission.append((team_name, problem_code, \
                         submit_time, result_code))
-    sys.stdout.write(json.dumps(submission, indent=4))
+    sys.stdout.write(json.dumps({"problem_count": problem_count, 
+            "submission": submission}, indent=4))
